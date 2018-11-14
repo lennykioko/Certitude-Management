9acfeecb2fce9c1777bc3146983e5fdd7880b6f7
lennykioko <lennykmutua@gmail.com>
2018-11-14 21:58:29 +0300

    Update debug and urls

--- certitude/settings.py
+++ certitude/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^27ey5tn5to143csw3s3eg0msjp(n%sbn*z-3%tl!5sqh!i!h7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
